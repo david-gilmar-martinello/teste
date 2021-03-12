26ba10ffd5ae9a12db212c465962226555c0040f
T.I <T.I@192.168.39.213>
2021-03-12 08:59:43 -0300

    Foi adicionado novas  classes

---
+++ src/main/java/Variavel.java
@@ -0,0 +1,28 @@
+/** Apresentar os conceitos de cariaveis
+*@author david.pereira
+*/
+public class Variavel{
+	
+	public static void main (String [] args) {
+		
+		String nome = "David";
+		String segundoNome, ultimoNome; // variavel sem valor definido
+ 		
+		int idade = 21; // variavel do tipo inteiro
+		boolean casado = true; // variavel do tipo verdadeiro ou falso
+		
+		
+		System.out.println(nome);
+		
+	}
+	
+}
+
+	/** Variavel
+	Variavel tem sempre que começar com letra minuscula
+	podem iniciar com _ ou $."porem não é recomendavel
+	Não pode ser iniciada por numeros
+	
+	o nome das variaveis tem que serem siguinificativos ou seja tem que ser comprencivel pelo usuario
+	não pode tbm utilizar nome de variavel com palavras chaves 
+	*/
---
+++ src/main/java/VariavelPrimitiva.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author T.I
+ */
+public class VariavelPrimitiva {
+
+    public static void main(String[] args) {
+        int idade = 21; //tipo inteiro não recebe fração
+
+        double preco = 12.45; // tipo double recebe numeração quebrada
+
+        char sexo = 'M'; // o char ele reconhce valores em letra e apenas uma letra entre aspas simples
+
+        boolean casado = true; // or false; boolean ele recebe apenas dois valores "verdadeiro ou falso"
+
+        byte b = 127; // o byte ele reconhece valores de até 127
+
+        //short s = 32768; // o short ele pode receber até 32768
+        int a = 2000000000; // o int pode recebe 32byte ou seja um tamanho de 2 bilhoes
+    }
+
+}
